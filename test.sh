f52313783ae510a8c6dcbd44cfbc375cf51cc541
Kevin Modzelewski <kevmod@gmail.com>
2022-08-09 22:29:42 +0000

    fix the fix

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 set -x
 
 sudo python3 -m pyperf system tune
-flock /tmp python3 -m pyperformance run --python ./python.nobolt --rigorous --manifest ~/pyston/macrobenchmarks/benchmarks/WEB_MANIFEST -o nobolt_macrobenchmarks.json -b djangocms,flaskblogging
-flock /tmp python3 -m pyperformance run --python ./python --rigorous --manifest ~/pyston/macrobenchmarks/benchmarks/WEB_MANIFEST -o bolt_macrobenchmarks.json -b djangocms,flaskblogging
+flock /tmp python3 -m pyperformance run --python ./python.nobolt --rigorous --manifest ~/pyston/pyston/macrobenchmarks/benchmarks/WEB_MANIFEST -o nobolt_macrobenchmarks.json -b djangocms,flaskblogging
+flock /tmp python3 -m pyperformance run --python ./python --rigorous --manifest ~/pyston/pyston/macrobenchmarks/benchmarks/WEB_MANIFEST -o bolt_macrobenchmarks.json -b djangocms,flaskblogging
 python3 -m pyperf compare_to {no,}bolt_macrobenchmarks.json
 
 flock /tmp python3 -m pyperformance run --python ./python.nobolt -o nobolt_pyperformance.json
